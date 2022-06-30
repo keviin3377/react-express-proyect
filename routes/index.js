d62e6761c723900ba989cda6f1275e53f5fb74a3
davethrasher1613 <daveangelofdeath@gmail.com>
2022-06-30 10:05:02 -0500

    backend prueba kevin

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,19 @@ const productosRouter=require('./productos.router')
 
 function routerApi(app){
 
-// mejora de rutas de endpoints
-
 const router= express.Router();
 app.use('/api/v1', router)
+//cors funcion para que se puedan hacer peticiones desde cualquier lugar
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 router.use('/envios',enviosRouter)
 router.use('/productos',productosRouter)
-
-
-
 }
-module.exports=routerApi
+
+module.exports=routerApi;
